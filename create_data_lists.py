17e0e11a10ff590cfcb19524c98d3228d2179618
daniel <yylaing@outlook.com>
2021-11-02 11:42:09 +0800

    1、修改文件夹路径

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,10 +1,10 @@
 from utils import create_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(train_folders=['/media/ssd/sr data/train2014',
-                                     '/media/ssd/sr data/val2014'],
-                      test_folders=['/media/ssd/sr data/BSDS100',
-                                    '/media/ssd/sr data/Set5',
-                                    '/media/ssd/sr data/Set14'],
+    create_data_lists(train_folders=['E:/source/MWDatabase/SR/SRGAN/COCO2014/train2014',
+                                     'E:/source/MWDatabase/SR/SRGAN/COCO2014/val2014'],
+                      test_folders=['E:/source/MWDatabase/SR/SRGAN/BSD100',
+                                    'E:/source/MWDatabase/SR/SRGAN/Set5',
+                                    'E:/source/MWDatabase/SR/SRGAN/Set14'],
                       min_size=100,
-                      output_folder='./')
+                      output_folder='./data/')
