cf7b519029687fe9726bb194fe3765934afa18b3
daniel <yylaing@outlook.com>
2021-11-02 15:40:03 +0800

    1、change dataset path

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,10 +1,10 @@
 from utils import create_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(train_folders=['E:/source/MWDatabase/SR/SRGAN/COCO2014/train2014',
-                                     'E:/source/MWDatabase/SR/SRGAN/COCO2014/val2014'],
-                      test_folders=['E:/source/MWDatabase/SR/SRGAN/BSD100',
-                                    'E:/source/MWDatabase/SR/SRGAN/Set5',
-                                    'E:/source/MWDatabase/SR/SRGAN/Set14'],
+    create_data_lists(train_folders=['../../../MWDatabase/SR/SRGAN/COCO2014/train2014',
+                                     '../../../MWDatabase/SR/SRGAN/COCO2014/val2014'],
+                      test_folders=['../../../MWDatabase/SR/SRGAN/BSD100',
+                                    '../../../MWDatabase/SR/SRGAN/Set5',
+                                    '../../../MWDatabase/SR/SRGAN/Set14'],
                       min_size=100,
                       output_folder='./data/')
